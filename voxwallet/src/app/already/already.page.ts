51c0c82989d35389d9b4e81c7304833b5fe418f7
Juan Suarez <juanluis.suarez@gmail.com>
2018-12-14 14:49:16 +0000

    - Small fix > overlay color - Some screens/pages

---
+++ voxwallet/src/app/already/already.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-already',
+  templateUrl: './already.page.html',
+  styleUrls: ['./already.page.scss'],
+})
+export class AlreadyPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- voxwallet/src/app/app-routing.module.ts
+++ voxwallet/src/app/app-routing.module.ts
@@ -6,7 +6,9 @@ import { AuthGuard } from './auth.guard';
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule', canActivate: [AuthGuard] },
   { path: 'welcome', loadChildren: './welcome/welcome.module#WelcomePageModule' },
-  { path: 'phone', loadChildren: './phone/phone.module#PhonePageModule' }
+  { path: 'terms', loadChildren: './terms/terms.module#TermsPageModule' },
+  { path: 'phone', loadChildren: './phone/phone.module#PhonePageModule' },
+  { path: 'already', loadChildren: './already/already.module#AlreadyPageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
