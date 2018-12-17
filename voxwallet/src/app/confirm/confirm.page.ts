4d6c621fc8f6c22c23079f05f74d7eb445c45736
Juan Suarez <juanluis.suarez@gmail.com>
2018-12-17 15:03:55 +0000

    - Confirm Screen

--- voxwallet/src/app/confirm/confirm.page.ts
+++ voxwallet/src/app/confirm/confirm.page.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { UserService, User } from '../user.service';
 
 @Component({
   selector: 'app-confirm',
   templateUrl: './confirm.page.html',
   styleUrls: ['./confirm.page.scss'],
 })
-export class ConfirmPage implements OnInit {
+export class ConfirmPage implements OnInit, AfterViewInit {
 
-  constructor() { }
+  // Models
+  confirmForm: FormGroup = null;
+
+  // Flags
+  isSending = false;
+
+  constructor(private router: Router,
+    private activatedRoute: ActivatedRoute,
+    public formBuilder: FormBuilder,
+    private userService: UserService) {
+
+    this.confirmForm = formBuilder.group({
+      seedWords: ['', Validators.compose([Validators.minLength(12), Validators.required])],
+      password: ['', Validators.compose([Validators.minLength(8), Validators.required])],
+      confirmPassword: ['', Validators.compose([Validators.minLength(8), Validators.required])]
+    });
+  }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.activatedRoute.params.subscribe((params) => { });
+  }
+
+  onConfirm() {
+    this.isSending = true;
+
+    const tmpModel: User = new User({
+    });
+
+    this.userService.saveUser(tmpModel);
+    this.router.navigate(['/selfie']);
   }
 
 }
